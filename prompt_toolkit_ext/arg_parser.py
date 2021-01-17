d89004d796fa469df046ecccd1269dd7cb74db34
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-01-17 09:38:24 +0800

    检测错误、清除错误标记时自动遍历所有子解析器

--- prompt_toolkit_ext/arg_parser.py
+++ prompt_toolkit_ext/arg_parser.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 class PromptArgumentParser(argparse.ArgumentParser):
@@ -17,9 +18,28 @@ class PromptArgumentParser(argparse.ArgumentParser):
         self.has_error = True
 
     def has_error_flag(self):
+
         if not hasattr(self, 'has_error'):
             self.has_error = False
+
+        if self.has_error:
+            return self.has_error
+
+        for sub_action in self._actions:
+            if isinstance(sub_action, argparse._SubParsersAction):
+                parser_map = sub_action._name_parser_map
+                for command, parser in parser_map.items():
+                    if isinstance(parser, PromptArgumentParser):
+                        if parser.has_error_flag():
+                            return True
+
         return self.has_error
 
     def clear_error_flag(self):
         self.has_error = False
+        for sub_action in self._actions:
+            if isinstance(sub_action, argparse._SubParsersAction):
+                parser_map = sub_action._name_parser_map
+                for command, parser in parser_map.items():
+                    if isinstance(parser, PromptArgumentParser):
+                        parser.clear_error_flag()
