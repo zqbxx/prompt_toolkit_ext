894121631c93ddcbb1823a637f9866f21ed0459c
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-01-23 11:07:19 +0800

    增加返回subparser和选项的方法

--- prompt_toolkit_ext/arg_parser.py
+++ prompt_toolkit_ext/arg_parser.py
@@ -43,3 +43,48 @@ class PromptArgumentParser(argparse.ArgumentParser):
                 for command, parser in parser_map.items():
                     if isinstance(parser, PromptArgumentParser):
                         parser.clear_error_flag()
+
+    def get_subparser_by_command(self, command, like=False):
+
+        subparsers = {}
+
+        for sub_action in self._actions:
+            if isinstance(sub_action, argparse._SubParsersAction):
+                if like:
+                    for c, p in sub_action._name_parser_map.items():
+                        if command in c:
+                            subparsers[c] = p
+                else:
+                    parser_map = sub_action._name_parser_map
+                    if parser_map.get(command):
+                        return parser_map.get(command)
+
+        if like:
+            if len(subparsers) > 0:
+                return subparsers
+
+        return None
+
+    def get_parser_opts(self, opt):
+        opt_list = []
+        for action in self._actions:
+            if isinstance(action, argparse.Action) and not isinstance(action, argparse._SubParsersAction):
+                opt_strings = action.option_strings
+                if opt in ['--', '-']:
+                    opt_list.append(opt_strings)
+                else:
+                    matched_opt_strings = []
+                    for opt_str in opt_strings:
+                        if opt in opt_str:
+                            matched_opt_strings.append(opt_str)
+                    opt_list.append(matched_opt_strings)
+
+        return opt_list
+
+    def get_subparsers(self):
+        subparser_list = []
+        for sub_action in self._actions:
+            if isinstance(sub_action, argparse._SubParsersAction):
+                parser_map = sub_action._name_parser_map
+                subparser_list.append(parser_map)
+        return subparser_list
